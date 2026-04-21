f53e751b65f4b1d969d34b087a4b3c9c70f2e796
Jean Staubley <staubleyjean@gmail.com>
2026-04-21 14:17:10 -0400

    Add files via upload

---
+++ test_phase_function.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def bhmie(x: float, refrel: complex, nang: int):
+    y = x * refrel
+    xstop = x + 4.0 * (x ** (1.0 / 3.0)) + 2.0
+    nstop = int(xstop)
+    ymod = abs(y)
+    nmx = int(max(xstop, ymod)) + 15
+    dang = (math.pi / 2.0) / float(nang - 1)
+    theta = [j * dang for j in range(nang)]
+    amu = [math.cos(t) for t in theta]
+    d = [0j] * (nmx + 1)   
+    d[nmx] = 0j
+    for n in range(1, nmx):  
+        rn = float(nmx - n + 1)
+        i = nmx - n
+        d[i] = (rn / y) - (1.0 / (d[i + 1] + rn / y))
+    pio = [0.0] * nang
+    pi1 = [1.0] * nang
+    nn = 2 * nang - 1
+    s1 = [0j] * nn
+    s2 = [0j] * nn
+    psi0 = math.cos(x)
+    psi1 = math.sin(x)
+    chi0 = -math.sin(x)
+    chi1 =  math.cos(x)
+    xi0 = complex(psi0, -chi0)
+    xi1 = complex(psi1, -chi1)
+    qsca = 0.0
+    for n in range(1, nstop + 1):
+        dn = float(n)
+        rn = dn
+        fn = (2.0 * rn + 1.0) / (rn * (rn + 1.0))
+        psi = (2.0 * dn - 1.0) * psi1 / x - psi0
+        chi = (2.0 * rn - 1.0) * chi1 / x - chi0
+        xi = complex(psi, -chi)
+        an_num = (d[n] / refrel + rn / x) * psi - psi1
+        an_den = (d[n] / refrel + rn / x) * xi  - xi1
+        an = an_num / an_den
+        bn_num = (refrel * d[n] + rn / x) * psi - psi1
+        bn_den = (refrel * d[n] + rn / x) * xi  - xi1
+        bn = bn_num / bn_den
+        qsca += (2.0 * rn + 1.0) * (abs(an) ** 2 + abs(bn) ** 2)
+        pi = [0.0] * nang
+        tau = [0.0] * nang
+        p = 1.0 if ((n - 1) % 2 == 0) else -1.0
+        t = 1.0 if (n % 2 == 0) else -1.0
+        for jF in range(1, nang + 1):  
+            j = jF - 1
+            jjF = 2 * nang - jF 
+            jj = jjF - 1
+            pi[j] = pi1[j]
+            tau[j] = rn * amu[j] * pi[j] - (rn + 1.0) * pio[j]
+            s1[jF - 1] += fn * (an * pi[j] + bn * tau[j])
+            s2[jF - 1] += fn * (an * tau[j] + bn * pi[j])
+            if jF != jjF:
+                s1[jj] += fn * (an * pi[j] * p + bn * tau[j] * t)
+                s2[jj] += fn * (an * tau[j] * t + bn * pi[j] * p)
+        psi0, psi1 = psi1, psi
+        chi0, chi1 = chi1, chi
+        xi1 = complex(psi1, -chi1)
+        if n < nstop:
+            rn_next = float(n + 1)
+            for j in range(nang):
+                pi1[j] = ((2.0 * rn_next - 1.0) / (rn_next - 1.0)) * amu[j] * pi[j] \
+                         - (rn_next / (rn_next - 1.0)) * pio[j]
+                pio[j] = pi[j]
+    qsca = (2.0 / (x * x)) * qsca
+    qext = (4.0 / (x * x)) * (s1[0].real)
+    qback = (4.0 / (x * x)) * (abs(s1[nn - 1]) ** 2)
+    return s1, s2, qext, qsca, qback
+
+def main():
+    print("\nSPHERE SCATTERING PROGRAM\n")
+    refmed = 1.0 # can change this
+    refre = 1.33257 # can change this
+    refim = 1.67e-8 # can change this
+    refrel = complex(refre, refim) / refmed
+    print(f"     REFMED = {refmed:8.4f}   REFRE ={refre:14.6E}   REFIM = {refim:14.6E}")
+    rad = 10 #in micro meter, can change this
+    wavel = 0.65 # in micro meter, can change this
+    x = 2.0 * math.pi * rad * refmed / wavel
+    print(f"     SPHERE RADIUS = {rad:7.3f}   WAVELENGTH = {wavel:7.4f}")
+    print(f"     SIZE PARAMETER ={x:8.3f}\n")
+    nang = 901 #changed from 11 to 901 to get resolution of 0.1 rad
+    s1, s2, qext, qsca, qback = bhmie(x, refrel, nang)
+    print(f"\n QSCA= {qsca:13.6E}   QEXT = {qext:13.6E}   QBACK = {qback:13.6E}")
+    print("\n  ANGLE       S11             POL             S33             S34\n")
+    s11nor = 0.5 * (abs(s2[0])**2 + abs(s1[0])**2)
+    nn = 2 * nang - 1
+    dang = (math.pi / 2.0) / float(nang - 1)
+    s11_list = [] # stores values across for loop to use in phase function
+    i1_list = [] # ''
+    i2_list = [] # ''
+    angles_list = [] # ''
+    for j in range(nn):
+        i1 = abs(s1[j])**2 # from the book: this is for perpendicular 
+        i2 = abs(s2[j])**2 # from the book: this is for parallel
+        s11 = 0.5 * abs(s2[j])**2 + 0.5 * abs(s1[j])**2
+        s12 = 0.5 * abs(s2[j])**2 - 0.5 * abs(s1[j])**2
+        pol = -s12 / s11
+        prod = s2[j] * s1[j].conjugate()
+        s33 = (prod.real) / s11
+        s34 = (prod.imag) / s11
+        s11n = s11 / s11nor
+        ang = dang * float(j) * 57.2958  
+        print(f" {ang:6.2f}  {s11n:13.6E}  {pol:13.6E}  {s33:13.6E}  {s34:13.6E}")
+        s11_list.append(s11) # appends the list to add new values
+        i1_list.append(i1) # ''
+        i2_list.append(i2) # ''
+        angles_list.append(ang) # ''
+    return s11_list, qsca, angles_list, i1_list, i2_list, rad, wavel
+
+def phase_function(s11_list, qsca, angles_list, i1_list, i2_list, rad, wavel):
+    s11_array = np.array(s11_list)
+    i1_array = np.array(i1_list)
+    i2_array = np.array(i2_list)
+    angles_array = np.array(angles_list)
+    a = rad
+    G = math.pi*(a**2)
+    csca = G*qsca
+    lambd = wavel
+    k = (2*math.pi)/lambd
+    dcsca = s11_array/(k**2)
+    p_unpolar = (4*math.pi/csca)*(dcsca)
+    norm_factor = ((4*math.pi)/csca)/(k**2)
+    p_perp = i1_array * norm_factor
+    p_para = i2_array * norm_factor
+    plt.figure(figsize=(9,6))
+    plt.plot(angles_array, p_unpolar, linestyle='-', color='black')
+    plt.plot(angles_array, p_perp, linestyle='-', color='blue')
+    plt.plot(angles_array, p_para, linestyle='-', color='red')
+    plt.yscale('log')
+    plt.title('Mie Scattering Phase Function')
+    plt.xlabel('Scattering Angle $\\theta$ (Degrees)')
+    plt.ylabel('Phase Function $p(\\theta)$')
+    plt.grid(True, which='both', ls='--', alpha=0.5)
+    plt.show()
+
+
+s11_list, qsca, angles_list, i1_list, i2_list, rad, wavel = main()
+phase_function(s11_list, qsca, angles_list, i1_list, i2_list, rad, wavel)
